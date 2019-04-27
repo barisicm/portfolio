40cc0aa23997d3ba94685c2ebfc3c7b30d56d36a
Marin Barišić <barisic.m@outlook.com>
2019-04-27 23:30:29 +0200

    Dodan razni tooling, nešto dokumentacije, i igranje sa animacijama.

---
+++ resources/scripts/application.js
@@ -0,0 +1,29 @@
+var button = document.getElementById("button");
+button.addEventListener("click", function () {
+    anime({
+        targets: '.block',
+        keyframes: [{
+                translateY: -40
+            },
+            {
+                translateX: 250
+            },
+            {
+                translateY: 40
+            },
+            {
+                translateX: 0
+            },
+            {
+                translateY: 0
+            }
+        ],
+        duration: 4000,
+        easing: 'easeOutElastic(1, .8)',
+        loop: true
+    });
+});
+
+const animeObject = anime({
+    /* Describe Animation */
+})
